64d1a6fb3fadb67497cba38bfb3e966348bb9296
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2022-12-02 19:31:32 +0000

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+
+       @include('layouts.navigation')
+
+
+        <div class="flex items-center justify-center">
+            <div class="flex flex-col justify-around">
+
+                <h1 class="text-4xl text-center text-gray-900">
+                    <span class="relative inline-block">
+                        <span class="relative">H</span>
+                        <span class="relative">e</span>
+                        <span class="relative">l</span>
+                        <span class="relative">l</span>
+                        <span class="relative">o</span>
+                        <span class="relative">,</span>
+                        <span class="relative"> </span>
+                        <span class="relative">W</span>
+                        <span class="relative">o</span>
+                        <span class="relative">r</span>
+                        <span class="relative">l</span>
+                        <span class="relative">d</span>
+                        <span class="relative">!</span>
+                    </span>
+                </h1>
+
+            </div>
+        </div>
+    </div>
+@endsection
